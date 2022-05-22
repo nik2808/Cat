b306714243431a5429ff706dbacb3a76523fb2a2
nik5693 <nikunj.madhogaria@sprinklr.com>
2022-05-22 21:15:58 +0530

    try arrow key

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ let cat = " cat.jpeg",
   beach = " bali-kelingking-beach-plastic-removal-drive.key",
   pitch = " NextByk Investor Pitch 2022.ppt",
   report = " interns-performance-report-may-2022.key";
-let x = 50,
-  y = 50,c=1;
+let c = 1;
 var sourc =
   "https://images.unsplash.com/photo-1561948955-570b270e7c36?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80";
 let s = cat;
@@ -16,54 +15,60 @@ document.getElementById("app").innerHTML = `
 <div class="container" id="container">
 <div class="lines">
 <ul class="no-bullets">
-<li class="cat"><button id="btn1" class="click"><img src="https://images.unsplash.com/photo-1561948955-570b270e7c36?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80" title:"cat.jpeg" style="height: 50px; min-width:50px; max-width:50px;"><div id="one"> ${cat}</div></button></li>
-<li class="couple"><button id="btn2" class="click"><img src="https://images.unsplash.com/photo-1606787620819-8bdf0c44c293?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80" style="height: 50px; min-width:50px; max-width:50px;"><div id="two"> ${couple}</div></button></li>
-<li class="beach"><button id="btn3" class="click"><img src="https://images.unsplash.com/photo-1539367628448-4bc5c9d171c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80" title:"cat.jpeg" style="height: 50px; min-width:50px; max-width:50px;"><div id="three"> ${beach}</div></button></li>
-<li class="pitch"><button id="btn4" class="click"><img src="https://images.unsplash.com/photo-1623206837956-07dab21608f6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80" title:"cat.jpeg" style="height: 50px; min-width:50px; max-width:50px;"><div id="four"> ${pitch}</div></button></li>
-<li class="report"><button id="btn5" class="click"><img src="https://images.unsplash.com/photo-1526628953301-3e589a6a8b74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80"" title:"cat.jpeg" style="height: 50px; min-width:50px; max-width:50px;"><div id="five"> ${report}</div></button></li>
+<li class="cat"><button id="btn1" class="click"><img src="https://images.unsplash.com/photo-1561948955-570b270e7c36?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80" title:"cat.jpeg" style="max-height: 50px; min-height: 50px; min-width:50px; max-width:50px;"><div id="one"> ${cat}</div></button></li>
+<li class="couple"><button id="btn2" class="click"><img src="https://images.unsplash.com/photo-1606787620819-8bdf0c44c293?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80" style="max-height: 50px; min-height:50px; min-width:50px; max-width:50px;"><div id="two"> ${couple}</div></button></li>
+<li class="beach"><button id="btn3" class="click"><img src="https://images.unsplash.com/photo-1539367628448-4bc5c9d171c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80" title:"cat.jpeg" style="max-height: 50px; min-height:50px; min-width:50px; max-width:50px;"><div id="three"> ${beach}</div></button></li>
+<li class="pitch"><button id="btn4" class="click"><img src="https://images.unsplash.com/photo-1623206837956-07dab21608f6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80" title:"cat.jpeg" style="max-height: 50px; min-height:50px; min-width:50px; max-width:50px;"><div id="four"> ${pitch}</div></button></li>
+<li class="report"><button id="btn5" class="click"><img src="https://images.unsplash.com/photo-1526628953301-3e589a6a8b74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80"" title:"cat.jpeg" style="max-height: 50px; min-height:50px; min-width:50px; max-width:50px;"><div id="five"> ${report}</div></button></li>
 </ul>
 </div>
 <div><ul>
-<li><img id="imag" src=${sourc} height="600" width="400"></li>
+<li><img id="imag" src=${sourc} height="800" width="500"></li>
 <li><textarea class="inpt" type="text" id="Txt" value=${s}>${s}</textarea></li></ul>
 </div>
 </div>
 `;
-
-var input = document.getElementById("Txt");
-const txt = document.getElementById("Txt");
-input.addEventListener("keypress", function(event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    var inp = document.getElementById("Txt").value;
-    if(c===1){
-      cat=inp;
-      document.getElementById("one").innerHTML=`${inp}`;
-    }
-    if(c===2){
-      couple=inp;
-      document.getElementById("two").innerHTML=`${inp}`;
-    }
-    if(c===3){
-      beach=inp;
-      document.getElementById("three").innerHTML=`${inp}`;
-    }
-    if(c===4){
-      pitch=inp;
-      document.getElementById("four").innerHTML=`${inp}`;
-    }
-    if(c===5){
-      report=inp;
-      document.getElementById("five").innerHTML=`${inp}`;
-    }
-  }
-});
 
 const btn1 = document.getElementById("btn1");
 const btn2 = document.getElementById("btn2");
 const btn3 = document.getElementById("btn3");
 const btn4 = document.getElementById("btn4");
 const btn5 = document.getElementById("btn5");
+
+document.addEventListener("keydown", function (event) {
+  if (event.keyCode === 38) {
+    btn1.click();
+  }
+});
+
+var input = document.getElementById("Txt");
+const txt = document.getElementById("Txt");
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    var inp = document.getElementById("Txt").value;
+    if (c === 1) {
+      cat = inp;
+      document.getElementById("one").innerHTML = `${inp}`;
+    }
+    if (c === 2) {
+      couple = inp;
+      document.getElementById("two").innerHTML = `${inp}`;
+    }
+    if (c === 3) {
+      beach = inp;
+      document.getElementById("three").innerHTML = `${inp}`;
+    }
+    if (c === 4) {
+      pitch = inp;
+      document.getElementById("four").innerHTML = `${inp}`;
+    }
+    if (c === 5) {
+      report = inp;
+      document.getElementById("five").innerHTML = `${inp}`;
+    }
+  }
+});
 
 const img = document.getElementById("imag");
 
@@ -76,7 +81,7 @@ btn1.addEventListener("click", function onClick() {
   btn3.style.backgroundColor = "white";
   btn4.style.backgroundColor = "white";
   btn5.style.backgroundColor = "white";
-  c=1;
+  c = 1;
 });
 
 btn2.addEventListener("click", function onClick() {
@@ -88,7 +93,7 @@ btn2.addEventListener("click", function onClick() {
   btn4.style.backgroundColor = "white";
   btn5.style.backgroundColor = "white";
   btn1.style.backgroundColor = "white";
-  c=2;
+  c = 2;
 });
 
 btn3.addEventListener("click", function onClick() {
@@ -101,7 +106,7 @@ btn3.addEventListener("click", function onClick() {
   btn4.style.backgroundColor = "white";
   btn5.style.backgroundColor = "white";
   btn1.style.backgroundColor = "white";
-  c=3;
+  c = 3;
 });
 
 btn4.addEventListener("click", function onClick() {
@@ -113,7 +118,7 @@ btn4.addEventListener("click", function onClick() {
   btn3.style.backgroundColor = "white";
   btn5.style.backgroundColor = "white";
   btn1.style.backgroundColor = "white";
-  c=4;
+  c = 4;
 });
 
 btn5.addEventListener("click", function onClick() {
@@ -125,5 +130,5 @@ btn5.addEventListener("click", function onClick() {
   btn3.style.backgroundColor = "white";
   btn4.style.backgroundColor = "white";
   btn1.style.backgroundColor = "white";
-  c=5;
+  c = 5;
 });
